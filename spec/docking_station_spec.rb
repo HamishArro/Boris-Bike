8509b3fa8b9c1ce71a0c2717f5a3e1be1c049519
Hamish Arro <Hamisharro@icloud.com>
2021-01-05 17:22:41 +0000

    Update docking_station_spec.rb

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,6 +4,6 @@ describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
 
-  it { expect(DockingStation.new.release_bike).is_a?(Bike) }
+  it { expect(DockingStation.new.release_bike).to respond_to :working? }
 
 end
