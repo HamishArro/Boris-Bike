61827aec490c38240a150f70d581a1a5e214d84f
megan gosnell <megan_gosnell@aol.co.uk>
2021-01-06 17:44:16 +0000

    up to chapter 16 completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,24 +1,30 @@
 require_relative './bike.rb'
 
 class DockingStation
-
+DEFAULT_CAPACITY = 20
   def initialize
     @station = Array.new
+
   end
 
   def release_bike
-    return fail "no bikes in station" unless bike?()
+    return fail "no bikes in station" unless !empty?()
     return @station.pop
   end
 
  def dock(bike)
+
+   return raise "docking station full" unless !full?()
    @station << bike
-   return raise "docking station full" unless @station.length <= 20
  end
 
- def bike?
-   return true unless @station.length == 0
-   return false
- end
-
+private
+def full?
+  return true unless @station.length == DEFAULT_CAPACITY
+  false
+end
+def empty?
+  return false unless @station.length == 0
+  true
+  end
 end
